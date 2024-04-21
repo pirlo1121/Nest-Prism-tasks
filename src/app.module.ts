43b0693a4a11e28b62afd89a49d158e7928f1f13
Maicol bautista <pirlo1121s@gmail.com>
2024-04-20 22:51:49 -0500

    {+} config e install prisma

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+// import { TaskModule } from './task/task.module';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
